8a5617a7c3899506e61a00293a6cb692113c4ebb
tylersussman15 <tylersussman15@gmail.com>
2025-10-04 02:27:54 -0400

    Compliant

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -3,3 +3,7 @@ if (loginSuccessful) {
 } else {
   logger.severe("User login failed for: " + username);
 }
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username)
+      ? username : "unauthorized user";
+}
