352de4017c9e39e3ef22fb1416937df37f163a37
tylersussman15 <tylersussman15@gmail.com>
2025-10-04 03:03:13 -0400

    Compliant

--- R011_TPS02_J.java
+++ R011_TPS02_J.java
@@ -1,58 +1,26 @@
-public final class SocketReader implements Runnable { // Thread-safe class
-  private final Socket socket;
-  private final BufferedReader in;
+public final class SocketReader implements Runnable {
+  private final SocketChannel sc;
   private final Object lock = new Object();
  
   public SocketReader(String host, int port) throws IOException {
-    this.socket = new Socket(host, port);
-    this.in = new BufferedReader(
-        new InputStreamReader(this.socket.getInputStream())
-    );
+    sc = SocketChannel.open(new InetSocketAddress(host, port));
   }
  
-  // Only one thread can use the socket at a particular time
   @Override public void run() {
+    ByteBuffer buf = ByteBuffer.allocate(1024);
     try {
       synchronized (lock) {
-        readData();
+        while (!Thread.interrupted()) {
+          sc.read(buf);
+          // ...
+        }
       }
     } catch (IOException ie) {
       // Forward to handler
     }
   }
- 
-  public void readData() throws IOException {
-    String string;
-    try {
-      while ((string = in.readLine()) != null) {
-        // Blocks until end of stream (null)
-      }
-    } finally {
-      shutdown();
-    }
-  }
- 
-  public void shutdown() throws IOException {
-    socket.close();
-  }
 }
  
 public final class PoolService {
-  private final ExecutorService pool;
- 
-  public PoolService(int poolSize) {
-    pool = Executors.newFixedThreadPool(poolSize);
-  }
- 
-  public void doSomething() throws InterruptedException, IOException {
-    pool.submit(new SocketReader("somehost", 8080));
-    // ...
-    List<Runnable> awaitingTasks = pool.shutdownNow();
-  }
- 
-  public static void main(String[] args)
-                          throws InterruptedException, IOException {
-    PoolService service = new PoolService(5);
-    service.doSomething();
-  }
+  // ...
 }
